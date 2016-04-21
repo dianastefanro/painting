8a2955c7787a82527d03bd902ad699c22dce358c
Diana <dianastefanro@gmail.com>
2016-04-21 22:23:08 +0300

    updates

--- includes.php
+++ includes.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- <link rel="shortcut icon" href="Images/favicon-hands.ico" type="image/x-icon"> -->
-<link rel="shortcut icon" href="Images/favicon-yoga-tree.ico" type="image/x-icon">
+<link rel="shortcut icon" href="Images/favicon-hands.ico" type="image/x-icon">
+<!-- <link rel="shortcut icon" href="Images/favicon-yoga-tree.ico" type="image/x-icon"> -->
 
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
